ae1bb01d9fb04a820269db42c774a70c7175af78
srijai16 <kattasrijai16@gmail.com>
2026-06-13 01:46:11 +0530

    deploy setup

--- frontend/src/lib/reservations-api.ts
+++ frontend/src/lib/reservations-api.ts
@@ -1,6 +1,4 @@
-const API_URL =
-  import.meta.env.VITE_API_URL ||
-  "http://localhost:8080";
+const API_URL ="https://inventory-reservation-system-eaas.onrender.com"
 
 
 function createIdempotencyKey() {
