33ae22524df7618efff03175c15d51ff5e01a743
srijai16 <kattasrijai16@gmail.com>
2026-06-13 01:10:30 +0530

    deploy setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://inventory-reservation-system-eaas.onrender.com",
         changeOrigin: true,
       },
     },
